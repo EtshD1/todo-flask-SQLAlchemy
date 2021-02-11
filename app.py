f0f5ebd9b32cea79902e8f3594813e1d919978f5
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-11 14:28:29 +0200

    config app

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etsh:3894@127.0.0.1:5432/todo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
