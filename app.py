21f8a8e4cd4d27a74dd679dcd1fda5186f972689
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-11 14:29:36 +0200

    render HTML template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,3 +8,12 @@ if __name__ == '__main__':
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etsh:3894@127.0.0.1:5432/todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=[
+        {'description': 'Todo 1'},
+        {'description': 'Todo 2'},
+        {'description': 'Todo 3'},
+    ])
