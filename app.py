6e655a1f37682644fce4ab15dabe8b9b1201face
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-13 10:36:28 +0200

    serve static folder

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-if __name__ == '__main__':
-    app.run()
+app = Flask(__name__, static_folder='public')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://etsh:3894@127.0.0.1:5432/todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,3 +25,7 @@ db.create_all()
 @app.route("/")
 def index():
     return render_template("index.html", data=Task.query.all())
+
+
+if __name__ == '__main__':
+    app.run()
