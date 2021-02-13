0148e6ba086bc4bd703198d02627efcabfd99686
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-13 13:12:50 +0200

    used chain methods instead of asynchronous functions

--- public/app.js
+++ public/app.js
@@ -3,20 +3,26 @@
   const form = document.querySelector("form");
 
   // On submit post to "/add"
-  form.addEventListener("submit", async (e) => {
+  form.addEventListener("submit", (e) => {
     // Disable refresh
     e.preventDefault();
     const desc = form["description"].value;
 
-    const response = await fetch('/add', {
+    fetch('/add', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: `description=${desc}`
-    });
-
-    const result = await response.json();
-    console.log(result);
+    })
+      .then(res => {
+        return res.json();
+      }).then(result => {
+        if (result.status === true) {
+          window.location.reload();
+        } else {
+          alert("Sorry and error occurred :(");
+        }
+      });
   });
 })()
